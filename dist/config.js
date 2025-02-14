e369bae94036297a8d94de8a506d514a8aeeec0b
siddharth <saumyaydv22@gmail.com>
2025-02-14 21:43:21 +0530

    first commit

--- dist/config.js
+++ dist/config.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.STATIC_USER = void 0;
 exports.STATIC_USER = {
     email: "admin@gmail.com",
-    password: "password123", // Use bcrypt in real apps
+    password: "password123",
 };
